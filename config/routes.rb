6c50dab845fd392d64257f68bbe414d265d81f74
medaminedev66 <md.amine.smahi@gmail.com>
2022-01-17 19:40:44 +0100

    Add an api for greetings

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  namespace :v1, defaults: {format: 'json'} do
+    get 'greetings', to: 'greetings#index'
+  end
 
   get '*page', to: 'statics#index', constraints: ->(req) do
     !req.xhr? && req.format.html?
